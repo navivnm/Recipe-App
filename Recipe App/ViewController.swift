565db15966a2759a960d9df4b29837ddf82520bf
Naveen Vijay <naveenvijay@Naveens-Mac.local>
2019-05-27 22:03:54 -0400

    add tableview, load data from array, display image

--- Recipe App/ViewController.swift
+++ Recipe App/ViewController.swift
@@ -11,15 +11,45 @@ import UIKit
 class ViewController: UIViewController
 {
     var recipeData = recipeClass()
+    @IBOutlet weak var tableViewRecipe: UITableView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("recipe app")
-        for i in recipeData.arrayRecipeName {
+        for i in recipeData.arrayRecipeName
+        {
             print(i)
         }
     }
 }
 
+extension ViewController: UITableViewDataSource
+{
+    //number of items displaying in tableview
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return recipeData.arrayRecipeName.count
+    }
+
+    
+    //display items in tableview
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableViewRecipe.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let image = UIImage(named: "kerala")
+        cell.imageView?.image = image
+        cell.textLabel?.text = recipeData.arrayRecipeName[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate
+{
+    //deselect selected row
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableViewRecipe.deselectRow(at: indexPath, animated: true)
+    }
+}
